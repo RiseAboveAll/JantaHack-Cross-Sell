2c4fd8308f6cf68f0fc6ce3545f8e9cf7117c15b
RiseAboveAll <44232787+RiseAboveAll@users.noreply.github.com>
2021-05-29 07:10:08 +0530

    Update missing_value.py

--- Scripts/missing_value.py
+++ Scripts/missing_value.py
@@ -1,28 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import numpy as np
 import pandas as pd
-
-
-# In[3]:
-
-
 def missingValue_Treat(df,col):
+    
     if df[col].dtypes.name=='object':
-        df[col].replace(np.nan, df[col].value_counts().sort_values(ascending=False)[0],inplace=True)
+        df[col].fillna(df[col].value_counts().sort_values(ascending=False).index[0],inplace=True)
     elif df[col].dtypes=='int64' or  df[col].dtypes=='int32':
-        df[col].replace(np.NAN, -999,inplace=True)
+        df[col].replace(np.nan, -999,inplace=True)
+    elif df[col].dtypes=='float64' or  df[col].dtypes=='float32':
+        df[col].replace(np.nan, -999.0,inplace=True)
     else:
-        df[col].replace(np.NAN, -999.0,inplace=True)
-        
-
-
-# In[ ]:
-
-
-
-
+        print("Error")
